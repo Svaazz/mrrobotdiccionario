964f8d502d2eab52574c4125b4969a88f4770e8c
Mike <Svaazz@users.noreply.github.com>
2016-09-04 14:40:08 +0200

    Update mrrobot.py

--- mrrobot.py
+++ mrrobot.py
@@ -60,7 +60,8 @@ class Diccionario:
 			archivo.write(self.pmayus(clave) + "\n")
 			archivo.write(self.umayus(clave) + "\n")
 			archivo.write(self.pumayus(clave) + "\n")
-
+			
+			#Repite, con numeros sustituyendo a vocales
 			archivo.write(self.vocales(clave) + "\n")
 			archivo.write(self.mayus(self.vocales(clave)) + "\n")
 			archivo.write(self.pmayus(self.vocales(clave)) + "\n")
@@ -69,7 +70,7 @@ class Diccionario:
 
 			if self.num == True:
 				for n in range(10000):
-					if n < 10:
+					if n < 10: # Añade un 0 delante del numero mientras que sea de una sola cifra
 						archivo.write(clave + '0' + str(n) + "\n")
 						archivo.write(self.mayus(clave) + '0' + str(n) + "\n")
 						archivo.write(self.pmayus(clave) + '0' + str(n) + "\n")
@@ -83,7 +84,7 @@ class Diccionario:
 						archivo.write(self.umayus(self.vocales(clave)) + '0' + str(n) + "\n")
 						archivo.write(self.pumayus(self.vocales(clave)) + '0' + str(n) + "\n")
 
-					archivo.write(clave + str(n) + "\n")
+					archivo.write(clave + str(n) + "\n") # De todas formas se escribe tambien el numero sin el cero delante para abarcar mas posibilidades
 					archivo.write(self.mayus(clave) + str(n) + "\n")
 					archivo.write(self.pmayus(clave) + str(n) + "\n")
 					archivo.write(self.umayus(clave) + str(n) + "\n")
@@ -101,16 +102,16 @@ class Diccionario:
 
 	def simple(self):
 		for i in range(3):
-			self.escribe(self.palabra[i])
+			self.escribe(self.palabra[i]) #Escribe las tres palabras tal cual
 	#Crea una contraseña uniendo las palabras dadas
 	def plano(self):
 		resultado = self.palabra[0] + self.palabra[1] + self.palabra[2]
 		self.escribe(resultado)
-
+	#Crea una contraseña uniendo las palabras dadas en orden inverso
 	def inverso(self):
 		resultado = self.palabra[2] + self.palabra[1] + self.palabra[0]
 		self.escribe(resultado)
-
+	#Construye una contraseña recortando las dos primeras letras de cada palabra y uniéndolas
 	def silabas(self):
 		si1 = self.palabra[0][:2]
 		si2 = self.palabra[1][:2]
@@ -118,10 +119,10 @@ class Diccionario:
 
 		resultado = si1 + si2 + si3
 		self.escribe(resultado)
-
+	#Si se usa -a crea contraseñas metiendo la conjunción en medio
 	def conjuncion(self):
 		if self.conj != '':
-			for i in range(0, 3):
+			for i in range(0, 3): #Intento abarcar la mayoría de posibilidades con 3 palabras
 				mezcla = self.palabra[i] + self.conj + self.palabra[i] + self.conj + self.palabra[i]
 				self.escribe(mezcla)
 			for i in range(0, 2):
@@ -142,8 +143,8 @@ class Diccionario:
 			for i in range(0, 2):
 				mezcla = self.palabra[i+1] + self.conj + self.palabra[i+1] + self.conj + self.palabra[i]
 				self.escribe(mezcla)
-
-			for i in range(0, 3):
+			
+			for i in range(0, 3): #Intento abarcar la mayoría de posibilidades con 2 palabras
 				mezcla = self.palabra[i] + self.conj + self.palabra[i]
 				self.escribe(mezcla)
 			for i in range(0, 2):
@@ -152,7 +153,7 @@ class Diccionario:
 			for i in range(0, 2):
 				mezcla = self.palabra[i] + self.conj + self.palabra[i+1]
 				self.escribe(mezcla)
-			
+	#Junta cada palabra con las dos otras y ella misma		
 	def juntar(self):
 		n = 0
 		while n <= 2:
@@ -160,7 +161,7 @@ class Diccionario:
 				resultado = self.palabra[n] + self.palabra[i]
 				self.escribe(resultado)
 			n += 1
-
+	#Cuenta cuantas lineas tiene el archivo para ver cuantas contraseñas se han generado
 	def contar(self):
 		os.chdir("Generated")
 		archivo = open(self.nombre, 'r')
@@ -172,7 +173,7 @@ class Diccionario:
 
 numeros = False
 conjun = ''
-
+#Esto es lo que interpreta los parametros
 if len(sys.argv) >= 5:
 	if sys.argv[4] == '-n':
 		numeros = True
@@ -185,10 +186,10 @@ if len(sys.argv) >= 5:
 			conjun = sys.argv[5]
 			numeros = True
 
-
 elif len(sys.argv) == 2:
 	if sys.argv[1] == '-h':
 		ayuda()
+
 
 try:
 	dicc = Diccionario(sys.argv[1], sys.argv[2], sys.argv[3], numeros, conjun)
